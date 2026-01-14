4d62a526bedaa0fa796a7e08ac5a5d678b95c44e
Katarzyna Kruk <kasia.kruk99@gmail.com>
2026-01-14 12:44:56 +0100

    5 fake restaurants created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.delete_all
+
+CATEGORIES = %w[chinese italian japanese french belgian]
+
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: CATEGORIES.sample
+  )
+end
+
+puts "Created #{Restaurant.count} restaurants!"
+
